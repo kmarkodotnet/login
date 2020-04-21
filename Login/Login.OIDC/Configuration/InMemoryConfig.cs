20da1cd2222316d5db42c1edbdacd4ff442d8c46
Markó Krisztián <kmarko@wsh.hu>
2020-04-21 10:50:45 +0200

    basics

---
+++ Login/Login.OIDC/Configuration/InMemoryConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Login.OIDC.Configuration
+{
+    public static class InMemoryConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+    }
+}
