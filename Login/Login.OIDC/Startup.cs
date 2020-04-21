713d954b7902d86970636150ab590d83ce77608a
Markó Krisztián <kmarko@wsh.hu>
2020-04-21 11:28:18 +0200

    working example based on: https://code-maze.com/identityserver4-integration-aspnetcore/

--- Login/Login.OIDC/Startup.cs
+++ Login/Login.OIDC/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Login.OIDC.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,11 @@ namespace Login.OIDC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer();
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                .AddTestUsers(InMemoryConfig.GetUsers())
+                .AddInMemoryClients(InMemoryConfig.GetClients())
+                .AddDeveloperSigningCredential(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
